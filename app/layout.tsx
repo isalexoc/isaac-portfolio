ad80f6ee650a822de96d2d215da1fa8445812728
Isaac Orraiz <isaacorraiz@gmail.com>
2024-07-20 23:21:44 -0400

    Projects Done

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en">
       <head>
         <link rel="icon" href="/jsm-logo.png" sizes="any" />
       </head>
